6a80b9a60fb4bf3bc4f06d0942a266bde39a16de
Jonathan <jonathanelian64@gmail.com>
2023-03-03 22:01:42 -0700

    test: add first test

---
+++ tests/javascript.rs
@@ -0,0 +1,10 @@
+use news::scrape::link::get_js_news;
+
+#[tokio::test]
+async fn test_get_js_news() {
+    let (news, options) = get_js_news("https://javascriptweekly.com/issues/628")
+        .await
+        .unwrap();
+    assert!(news.len() > 0, "News vector must be with items");
+    assert!(options.len() > 0, "Potions vector must be with items");
+}
