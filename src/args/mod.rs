1c2cf8a25b89031d1dc625a0eceebb3536f8aba2
Jonathan <jonathanelian64@gmail.com>
2023-03-15 10:03:01 -0700

    feat: new version 0.1.0

--- src/args/mod.rs
+++ src/args/mod.rs
@@ -3,7 +3,7 @@ use clap::{ArgMatches, Command};
 pub fn get_args() -> ArgMatches {
     let opts = Command::new("news")
         // creamos las optiones de la app
-        .version("0.0.2")
+        .version("0.1.0")
         .author("Jonathan @Atticus64 in Github")
         .about("CLI to watch news in the terminal 🐢")
         .get_matches();
