37dbe8cd63410b5636c0ec48fcfb0c130057080f
Jonathan <jonathanelian64@gmail.com>
2023-06-13 09:57:23 -0700

    docs: change package name

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 //! # News
 //!
 //! `News` is a cli program to watch developer news from the terminal
-use news::get_news;
+use news_cli::get_news;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
